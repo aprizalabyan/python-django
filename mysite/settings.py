442168cb1e3300a61615f0eb92473b0fda705bac
aprizalabyan <abi@jkt1.ebdesk.com>
2024-02-23 11:05:34 +0700

    add playground-app, templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-prh3ike0(s=dv@qr_a3^w6$2bmjg-899f8pf17-kt5u8v=cnw+'
+SECRET_KEY = 'django-insecure-(8s)lh#)cw60&!byt&&4s56_#=3_=70*+0!!g*2425j8jt8kv#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'playground',
 ]
 
 MIDDLEWARE = [
